2ff22271093ffe0c945d7f46477498339af257f3
Safei Ashraf <safei.ashraf@gmail.com>
2022-11-13 10:18:23 +0200

    feat: ch3 solution

--- ch3.js
+++ ch3.js
@@ -35,3 +35,26 @@ const countBs = (word, letter = "B") => {
 const countChar = (word, letter) => {
 	return countBs(word, letter);
 };
+
+//Recursion
+
+/* We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to
+see whether it’s divisible by two.Here’s another way to define whether a positive whole number is even or odd:
+
+Zero is even.
+
+One is odd.
+
+For any other number N, its evenness is the same as N - 2.
+
+Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+*/
+
+const isEven = (num) => {
+	if (num === 0) return true;
+	if (num === 1 || num < 0) return false;
+	if (num < 0) return isEven(-num);
+	return isEven(num - 2);
+};
